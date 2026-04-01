1d0114eae57ded53a820d02c4913c4dd03de41cd
aditya <adityasarda08.com>
2026-04-01 14:15:20 +0530

    code updated

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,28 @@
-
 import React from "react";
 import './../styles/App.css';
+import { BrowserRouter, Route, Switch, Link } from "react-router-dom";
+import Home from "./Home";
+import About from "./About";
 
 const App = () => {
   return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
-}
+    <BrowserRouter>
+      <div>
+        <ul>
+          <li>
+              <Link to="/">Home</Link>
+          </li>
+          <li>
+              <Link to="/about">About</Link>
+            </li>
+        </ul>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+        </Switch>
+      </div>
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
